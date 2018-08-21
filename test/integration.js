6baa46e54ab5cd12a28d853704a8cd511b403492
Alexander Lichter <manniL@gmx.net>
2018-08-21 22:56:40 +0100

    fix: drop snapshots

--- test/integration.js
+++ test/integration.js
@@ -5,17 +5,18 @@ import fetchMock from 'fetch-mock'
 import { promisify } from 'util'
 import { init } from '../src/init'
 import fetch from 'node-fetch'
+import { formatMoney } from '../src/utils/misc'
 
 test.serial('it prints everything', async t => {
   const logo = 'You are beautiful!'
   const stats = {
-    'slug': 'nuxtjs',
-    'currency': 'USD',
-    'image': 'https://opencollective-production.s3-us-west-1.amazonaws.com/251e1a10-369b-11e7-8ad6-5967d7493bb7.png',
-    'balance': 435949,
-    'yearlyIncome': 2528815,
-    'backersCount': 110,
-    'contributorsCount': 129
+    slug: 'nuxtjs',
+    currency: 'USD',
+    image: 'https://opencollective-production.s3-us-west-1.amazonaws.com/251e1a10-369b-11e7-8ad6-5967d7493bb7.png',
+    balance: 435949,
+    yearlyIncome: 2528815,
+    backersCount: 110,
+    contributorsCount: 129
   }
 
   // Assign as the assignment in init happens after mocking
@@ -34,7 +35,17 @@ test.serial('it prints everything', async t => {
 
   await init(pkgPaths.full)
 
-  t.snapshot(log)
+  t.true(log.includes(logo))
+  t.true(log.includes('Thanks for installing fake 🙏'))
+  t.true(log.includes('Please consider donating to our open collective'))
+  t.true(log.includes('to help us maintain this package.'))
+  t.true(log.includes(`Number of contributors: ${stats.contributorsCount}`))
+  t.true(log.includes(`Number of backers: ${stats.backersCount}`))
+
+  const formatInCurrency = formatMoney(stats.currency)
+
+  t.true(log.includes(`Annual budget: ${formatInCurrency(stats.yearlyIncome)}`))
+  t.true(log.includes(`Current balance: ${formatInCurrency(stats.balance)}`))
 })
 
 test.serial('it runs the postinstall script after npm install', async t => {
@@ -47,5 +58,8 @@ test.serial('it runs the postinstall script after npm install', async t => {
     }
   })
   const stdout = rawStdout.toString('utf8').split('\n').slice(9, 13).join('\n')
-  t.snapshot(stdout)
+
+  t.true(stdout.includes('Thanks for installing fake 🙏'))
+  t.true(stdout.includes('Please consider donating to our open collective'))
+  t.true(stdout.includes('to help us maintain this package.'))
 })
