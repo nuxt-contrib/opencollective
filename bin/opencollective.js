744ce37eb03bf2bad866f3c517a5eaff75b8fd63
Pooya Parsa <pyapar@gmail.com>
2018-12-12 21:45:10 +0330

    fix: force process exit in case of errors

--- bin/opencollective.js
+++ bin/opencollective.js
@@ -2,4 +2,4 @@
 
 require('../dist/opencollective.cjs')
   .init(process.argv.length > 2 ? process.argv[2] : '.')
-  .catch(console.error) // eslint-disable-line no-console
+  .catch(process.exit(1))
