ed2efb076f5c75a11df96a90a6beb3a95920ec10
Yannick Schurter <yannick.schurter@gmail.com>
2019-03-19 17:41:01 +0100

    fix: properly hide message (#6)

--- src/init.js
+++ src/init.js
@@ -4,6 +4,10 @@ import { getCollective } from './utils/transforms'
 import { hideMessage } from './utils/misc'
 
 export async function init (path, hide = hideMessage()) {
+  if (hide) {
+    return
+  }
+
   global.fetch = global.fetch || fetch
   const collective = await getCollective(path)
 
