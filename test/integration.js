c06113a8ffcef070f209a6275dc20569cb0c7d83
Yannick Schurter <yannick.schurter@gmail.com>
2019-03-19 17:04:20 +0100

    add test coverage for hide var

--- test/integration.js
+++ test/integration.js
@@ -9,26 +9,18 @@ import { formatMoney } from '../src/utils/misc'
 import pkgJsonFull from './fixtures/package-full/package'
 import { retrieveCols } from '../src/utils/print'
 
-test.serial('it prints everything', async t => {
-  const logo = 'You are beautiful!'
-  const stats = {
-    slug: 'nuxtjs',
-    currency: 'USD',
-    image: 'https://opencollective-production.s3-us-west-1.amazonaws.com/251e1a10-369b-11e7-8ad6-5967d7493bb7.png',
-    balance: 435949,
-    yearlyIncome: 2528815,
-    backersCount: 110,
-    contributorsCount: 129
-  }
+const logo = 'You are beautiful!'
+const stats = {
+  slug: 'nuxtjs',
+  currency: 'USD',
+  image: 'https://opencollective-production.s3-us-west-1.amazonaws.com/251e1a10-369b-11e7-8ad6-5967d7493bb7.png',
+  balance: 435949,
+  yearlyIncome: 2528815,
+  backersCount: 110,
+  contributorsCount: 129
+}
 
-  const formatInCurrency = formatMoney(stats.currency)
-
-  const pkgCollective = pkgJsonFull.collective
-  const url = pkgCollective.url + pkgCollective.donation.slug + '/' + pkgCollective.donation.amount
-
-  const cols = retrieveCols()
-  const printedSpaces = ' '.repeat(cols / 2)
-
+test.before(t => {
   // Assign as the assignment in init happens after mocking
   global.fetch = fetch
   fetchMock.mock('https://opencollective.com/fake.json', stats)
@@ -36,6 +28,20 @@ test.serial('it prints everything', async t => {
     body: logo,
     headers: { 'content-type': 'text/plain' }
   })
+})
+
+test.beforeEach(t => {
+  fetchMock.resetHistory()
+})
+
+test.serial('it prints everything', async t => {
+  const formatInCurrency = formatMoney(stats.currency)
+
+  const pkgCollective = pkgJsonFull.collective
+  const url = pkgCollective.url + pkgCollective.donation.slug + '/' + pkgCollective.donation.amount
+
+  const cols = retrieveCols()
+  const printedSpaces = ' '.repeat(cols / 2)
 
   let log = ''
   process.stdout.write = (write => function (string, encoding, fileDescriptor) {
@@ -60,6 +66,19 @@ test.serial('it prints everything', async t => {
   ]
 
   content.forEach(c => t.true(log.includes(c)))
+})
+
+test.serial('it prints nothing when hide is true', async t => {
+  let log = ''
+  process.stdout.write = (write => function (string, encoding, fileDescriptor) {
+    log += string
+    write.apply(process.stdout, arguments)
+  })(process.stdout.write)
+
+  await init(pkgPath, true)
+
+  t.false(fetchMock.called())
+  t.is(log.trim(), '')
 })
 
 test.serial('yarn postinstall script works as expected', async t => {
