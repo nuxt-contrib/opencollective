87154c41b78dacc0b2e039db1f1bf768e2d390ce
anupamme <mediratta@gmail.com>
2026-08-27 11:21:57 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- bin/opencollective.js
+++ bin/opencollective.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
+const path = require('node:path')
+
+const targetPath = process.argv.length > 2 ? process.argv[2] : '.'
+const cwd = process.cwd()
+const resolvedPath = path.resolve(cwd, targetPath)
+
+if (resolvedPath !== cwd && !resolvedPath.startsWith(cwd + path.sep)) {
+  console.error(`Invalid path argument: "${targetPath}" resolves outside of the current working directory`)
+  process.exit(1)
+}
+
 require('../dist/index.js')
-  .init(process.argv.length > 2 ? process.argv[2] : '.')
+  .init(resolvedPath)
   .then(() => process.exit(0))
   .catch(() => process.exit(1))
